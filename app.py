d2f67514fe42841747a60181c8b23584b5e48d48
kennykabulo2-png <kennykabulo2@gmail.com>
2026-06-12 16:34:40 +0200

    AGENT IA FINANCE ET ECONOMIE

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, session, redirect, url_for
+    from flask import Flask, jsonify, request, session, redirect, url_for
 from functools import wraps
 import json
 import hashlib
